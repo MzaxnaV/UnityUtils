39273bf5288fb67b94a7eee119a8239ab535470e
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-09-19 11:04:48 +0100

    add assembly def and fix package

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,35 +1,41 @@
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+namespace UnityUtils {
 
-    private static T _instance;
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
 
-    public static T Instance
-    {
-        get
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+            
+                _instance = FindObjectOfType<T>();
+
+                if (_instance != null) return _instance;
+            
+                var singletonObject = new GameObject(typeof(T).Name);
+                _instance = singletonObject.AddComponent<T>();
+                DontDestroyOnLoad(singletonObject);
+            
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
         {
             if (_instance == null)
             {
-                GameObject singletonObject = new GameObject(typeof(T).Name);
-                _instance = singletonObject.AddComponent<T>();
-                DontDestroyOnLoad(singletonObject);
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
             }
-            
-            return _instance;
+            else
+            {
+                Destroy(gameObject);
+            }
         }
-    }
 
-    protected virtual void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
     }
-
 }
